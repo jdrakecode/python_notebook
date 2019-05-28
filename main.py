e3c2f4a9dbc5892f40ae688b37c3b8cd3a8b4e23
jdrakecode <jdrakepaypal@gmail.com>
2019-05-28 14:03:50 -0400

    got the note feature working, the obj is constructed

---
+++ main.py
@@ -0,0 +1,25 @@
+from datetime import date
+"""
+Note Book app
+
+Notes will have three parts
+ID
+Content
+Date/Time
+"""
+
+notebook = []
+
+counter = 1
+note_id = counter
+
+
+counter += 1
+
+now = date.today()
+
+content = "This is content"
+
+note = (note_id, str(now), content)
+notebook.append(note)
+print(notebook)
