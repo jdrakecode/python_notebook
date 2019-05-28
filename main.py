e9a5b15612a758e52f3aeaa4243a92a790da7650
jdrakecode <jdrakepaypal@gmail.com>
2019-05-28 14:36:10 -0400

    yet another

--- main.py
+++ main.py
@@ -11,10 +11,23 @@ counter = 1
 notebook = []
 now = date.today()
 
-while counter <=5:
-    content = input("What is the note \n> ")
-    note_id = counter
-    note = (note_id, str(now), content)
-    notebook.append(note)
-    counter += 1
-    print(notebook)
+while True:
+    user_response = input("What would you like to do \n"
+                        "1. Add a note \n"
+                        "2. Print a note \n"    
+                        "3. Exit\n> ")
+
+    if user_response == "1":
+        content = input("What is the note \n> ")
+        note_id = counter
+        note = (note_id, str(now), content)
+        notebook.append(note)
+        counter += 1
+    elif user_response == "2":
+        for note in notebook:
+            print(f"ID: {note[0]}|Note: {note[2]}")
+    elif user_response == "3":
+        exit()
+    else:
+        print("Invalid input")
+
